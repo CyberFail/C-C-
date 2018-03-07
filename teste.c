a8773544f358abd27f0d8371d2c905d3c9834ddc
Matheus Rodrigues <matheus.rodriguesoliveira@hotmail.com>
2018-03-07 09:41:41 -0300

    TesteSimples
    Simples teste em c

--- teste.c
+++ teste.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
 
-    printf("MAtheus\n");
+    int o;
+    do{
+
+    printf("*** Menu ***\n");
+    printf(" 1 - Print seu nome\n");
+    printf(" 2 - Print sua idade");
+    printf(" 0 - Sair\n");
+
+
+    printf("Digite sua opção: ");
+    scanf("%d", o);
+
+    switch(o){
+        case 0:
+            printf("Saindo...");
+            print("clear")
+        break;
+
+        case 1:
+            char nome;
+            printf("Digite seu nome: \n");
+            scanf("%c", nome);
+            printf("Bem vindo" + nome);
+        break;
+
+        case 2:
+            int i;
+            printf("Digite sua idade: \n");
+            scanf("%d", i);
+        break;
+    }
+
+
+    }while(o);
 
 }
